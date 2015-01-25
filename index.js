c8b8ab60ecf2d373c3b7dec34de4b597ea8196b1
Rollie Ma <rollie.ma@gmail.com>
2015-01-24 23:53:28 -0800

    Https request to Slack incoming webhook test

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 
+var https = require('https');
 var CheckEvent = require('../../models/checkEvent');
+
+// https://hooks.slack.com/services/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
 
 exports.initWebApp = function(options) {
     var config = options.config.slack;
 
     CheckEvent.on('afterInsert', function(checkEvent) {
+        if (!config.event[checkEvent.message]) {
+            return;
+        }
+        checkEvent.findCheck(function(err, check) {
+            if (err) {
+                return console.error(err);
+            }
+            var options = {
+                hostname: 'hooks.slack.com',
+                port: 443,
+                path: '/services/T03EH2H3P/B03EH2NRH/zGLLVCqDnoI265x0KXMzOFfN',
+                method: 'POST'
+            };
 
+            var req = https.request(options, function(res) {
+                console.log('STATUS: ' + res.statusCode);
+                console.log('HEADERS: ' + JSON.stringify(res.headers));
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    console.log('BODY: ' + chunk);
+                });
+            });
+
+            req.on('error', function(e) {
+                console.log('problem with request: ' + e.message);
+            });
+
+            // write data to request body
+            req.write(JSON.stringify({
+                channel: '#general',
+                username: 'Uptime Bot',
+                text: 'Test test test.',
+                icon_emoji: ':ghost:'
+            }));
+            req.end();
+        });
     });
+
+    console.log('Enabled Email notifications');
 }
